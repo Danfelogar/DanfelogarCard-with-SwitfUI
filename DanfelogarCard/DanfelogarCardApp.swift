f984dbc66a8776109e107544c3c70796926c2847
Danfelogar <danypolo18@hotmail.com>
2023-09-27 00:13:48 -0500

    Initial Commit

---
+++ DanfelogarCard/DanfelogarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanfelogarCardApp.swift
+//  DanfelogarCard
+//
+//  Created by Daniel Felipe on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DanfelogarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
